326a40af4d14263725fbb2d4391ee6220349e914
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2023-05-03 23:58:16 +0200

    Redirección relativa (no localhost)

--- client/src/index.js
+++ client/src/index.js
@@ -10,9 +10,11 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Navegacion from './components/Plantilla/Navegacion';
 import Footer from './components/Plantilla/Footer';
 import AbsoluteRedirect from './utils/AbsoluteRedirect';
+import RedirectTo from './utils/RedirectTo';
 import { isLogin } from './utils/JWT';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Error404 from './components/Error404';
+import Modificar from './paginas/Modificar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,7 +26,7 @@ root.render(
         (
             <Switch>
                 <Route exact path='/' component={App}></Route>
-                <Route exact path='/login' render={() => <AbsoluteRedirect to={"http://localhost:8090/restaurantes"} />}></Route>                
+                <Route exact path='/login' render={() => <RedirectTo puerto="8090" ruta="restaurantes" />}></Route>                
                 
                 <Route path='/' render={() => (<Redirect to="/" />)}></Route>
             </Switch>
@@ -34,6 +36,7 @@ root.render(
                 <Route exact path="/restaurantes" component={App}></Route>
                 <Route exact path='/crear' component={Crear}></Route>
                 <Route exact path='/restaurantes/:id' render={(props) => <Restaurante id={props.match.params.id} />}></Route>
+                <Route exact path='/restaurantes/:id/modificar' render={(props) => <Modificar id={props.match.params.id} />}></Route>
                 <Route exact path='/opiniones/:id' render={(props) => <Opinion id={props.match.params.id} />}></Route>
                 <Route exact path='/login' render={() => <Redirect to="/" />}></Route>
                 <Route path='/' component={Error404}></Route>
