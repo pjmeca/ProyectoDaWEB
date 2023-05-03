24a0710540f71f04bfd5bb882ed1f5b2e53dcc50
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2023-05-03 23:59:48 +0200

    SitiosTuristicos seleccionables si editable=true

--- client/src/components/Tablas/TablaSitiosTuristicos.js
+++ client/src/components/Tablas/TablaSitiosTuristicos.js
@@ -1,15 +1,60 @@
 import Table from "react-bootstrap/Table";
+import { Form } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
 
-export default function TablaSitiosTuristicos({sitiosTuristicos}) { // TODO verificar funcionamiento
+export default function TablaSitiosTuristicos({sitiosTuristicos, editable=false, onSeleccionadosChange}) {
 
-  function ContenidoTabla() {
+  // Ignora el warning de uncontrolled input (no afecta a la funcionalidad)
+  useEffect(() => {
+    const originalConsoleError = console.error;
+    console.error = (...args) => {
+      if (
+        typeof args[0] === "string" &&
+        args[0].startsWith("Warning: A component is changing an uncontrolled input")
+      ) {
+        return;
+      }
+      originalConsoleError.call(console, ...args);
+    };
+    return () => {
+      console.error = originalConsoleError;
+    };
+  }, []);
+
+  const [seleccionados, setSeleccionados] = useState([])
+
+  useEffect(() => {
+    let tam = typeof sitiosTuristicos === 'undefined' ? 0 : sitiosTuristicos.length
+    setSeleccionados(new Array(tam).fill(true));  
+  }, [sitiosTuristicos])
+
+  function ContenidoTabla() {   
+
     return sitiosTuristicos.map((sitiosTuristico, i) => (
       <tr key={i}>
         <th style={{width: "50%"}}>{sitiosTuristico.nombre}</th>
-        <th style={{width: "50%"}}>{sitiosTuristico.wikipediaUrl}</th>
-      
+        <th style={{width: "50%"}}>{sitiosTuristico.descripcion}</th>
+        {editable ? 
+               	<th>
+                  <Form.Check
+                    type="checkbox"
+                    checked={seleccionados[i]}
+                    onChange={(event) => {
+                      const aux = [...seleccionados];
+                      aux[i] = event.target.checked;
+                      setSeleccionados(aux)
+                    }}
+                  />
+                </th> :
+                <></>
+              }
       </tr>
     ));
+  }
+
+  function handleActualizar() {
+    onSeleccionadosChange(seleccionados)
   }
 
   return (
@@ -18,16 +63,26 @@ export default function TablaSitiosTuristicos({sitiosTuristicos}) { // TODO veri
         <></>
       ) : (
         <>
-        <h3>Platos</h3>
+        <h3>Sitios Turísticos</h3>
         <Table striped responsive="xl">
           <thead>
             <tr>
               <th>Nombre</th>
-              <th>Más información</th>
+              <th>Descripción</th>
+              {editable ? 
+               	<th>Seleccionados</th> :
+                <></>
+              }
             </tr>
           </thead>
           <tbody>{ContenidoTabla()}</tbody>
         </Table>
+        {editable ?
+          <Button variant="primary" type="submit" onClick={handleActualizar}>
+            Actualizar
+          </Button> :
+          <></>
+        }
         </>
       )}
     </div>
