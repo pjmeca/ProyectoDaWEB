9ad8fbfb0ad7d0e3b3f63aa719c376881d421b75
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2023-05-28 01:11:56 +0200

    Simplificación
    Eliminado código duplicado usando un valor por defecto en "imagen".

--- client/src/components/Plantilla/ImagenHeader.js
+++ client/src/components/Plantilla/ImagenHeader.js
@@ -1,23 +1,14 @@
-export default function ImagenHeader({ titulo, imagen }) {
+export default function ImagenHeader({ titulo, imagen="/assets/img/calle.jpg" }) {
   titulo = titulo ? titulo : "Restaurantes";
 
   return (
     <>
-      {imagen ? (
-        <div
-          className="cabecera"
-          style={{ backgroundImage: "url(" + { imagen } + ")" }}
-        >
-          <p>{titulo}</p>
-        </div>
-      ) : (
-        <div
-          className="cabecera"
-          style={{ backgroundImage: "url(/assets/img/calle.jpg)" }}
-        >
-          <p>{titulo}</p>
-        </div>
-      )}
+      <div
+        className="cabecera"
+        style={{ backgroundImage: "url(" + imagen + ")" }}
+      >
+        <p>{titulo}</p>
+      </div>
     </>
   );
 }
